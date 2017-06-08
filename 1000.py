7d4dabc22b1aa9e0b4bce7e5a76b02e2ca5a274a
rfgtkmrf <j_home@mail.ru>
2017-06-08 19:37:46 +0500

    button2

--- 1000.py
+++ 1000.py
@@ -1,10 +1,10 @@
 import sys
 from PyQt5.QtWidgets import (QWidget, QToolTip,
-    QPushButton, QApplication, QDesktopWidget)
+    QPushButton, QApplication, QDesktopWidget, QMainWindow, QLabel)
 from PyQt5.QtGui import (QFont, QIcon)
 from PyQt5.QtCore import QCoreApplication
 
-class Example(QWidget):
+class Example(QMainWindow):
 
     def __init__(self):
         super().__init__()
@@ -16,7 +16,7 @@ class Example(QWidget):
         self.setToolTip('Тут <i><b>нет</b></i> кнопки.')
 
         btn = QPushButton('Button', self)
-        btn.clicked.connect(self.Button)
+        btn.clicked.connect(self.buttonClicked)
         btn.setToolTip('Да, вот она, <b>кнопочка</b> :-)')
         btn.resize(btn.sizeHint())
         btn.move(50, 50)
@@ -27,7 +27,7 @@ class Example(QWidget):
         qbtn.setToolTip('А это выход!')
         qbtn.resize(qbtn.sizeHint())
         qbtn.move(150, 50)
-        
+               
         self.setGeometry(250, 200, 400, 300)
         self.setWindowTitle('Програмка же!')
         self.setWindowIcon(QIcon('player.png'))
@@ -40,13 +40,14 @@ class Example(QWidget):
         qr.moveCenter(cp)
         self.move(qr.topLeft())
         
-    def Button(self):
-        reply = QMessageBox.question(self, 'Вопрос, однако', 'Точно?', QMessageBox.Yes, QMessageBox.No)
-        if reply == QMessageBox.Yes:
-            pass
-        else:
-            pass
-            
+    def buttonClicked(self):
+        sender = self.sender()
+        self.statusBar().showMessage(sender.text() + ' was pressed')
+        
+        lbl1 = QLabel('Zetcode', self)
+        lbl1.move(15, 10)
+        self.show()
+        
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
